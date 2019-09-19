7cb37df12e5d7cc7584456de18969bedb23c12bc
Mrdayday <51329421+Mrdayday@users.noreply.github.com>
2019-09-19 16:51:35 +0200

    Update Software.ino

--- Software/Software.ino
+++ Software/Software.ino
@@ -1,4 +1,4 @@
-//codeing partly by Martin Ortner
+//codeing by Martin Ortner and Jan Stotz
 #include <ModbusRtu.h> //Modbus libary (Modbus Master-Slave library for Arduino by smarmengol(Github)
 // data Array for Modbus excange
 /*
@@ -6,21 +6,24 @@
  */
 uint16_t pumpdata[1]={0};
 // define Used Pins
+// define pin to switch on the pump
 #define sourcePin 3
-
+// define pin that is connected to the button for priming the tubes
 #define buttonPin 5
 // assign the Arduino pin that must be connected to RE-DE RS485 transceiver
 #define TXEN  4
 // define Slave 2
 Modbus slave(2,0,TXEN);
-
+//initialize on_time as 0 so that the pump is not running at the start
 unsigned long on_time = 0;
+
 volatile bool switch_on = true;
 int run_val;
 
 
 void setup() {
   slave.begin(19200); // baud-rate at 19200
+  // define pintype
   pinMode(sourcePin, OUTPUT);
   pinMode(buttonPin, INPUT);
   }
@@ -29,13 +32,13 @@ void loop() {
   //update array info
   Start:slave.poll(pumpdata,1);
   
-  if (digitalRead(buttonPin)==1) {
+  if (digitalRead(buttonPin)==1) { //ask if the button is pressed
     digitalWrite(sourcePin, 1);
-    goto Start;
+    goto Start; //go to beginning of the loop
   }
   
   if (pumpdata[0] > 0 && switch_on == true){
-    on_time = millis();
+    on_time = millis();//get new time
     run_val = pumpdata[0];
     digitalWrite(sourcePin, HIGH);
     switch_on = false;
@@ -45,8 +48,9 @@ void loop() {
     switch_on = true;
     pumpdata[0] = 0;
   }
+  //count the second downwards
   if (millis() >= on_time + 1000 && pumpdata[0] != 0){
-    on_time = millis();
-    -- pumpdata[0];
+    on_time = millis();//get new time
+    -- pumpdata[0];//reduce the businformation by one
   }
 }
